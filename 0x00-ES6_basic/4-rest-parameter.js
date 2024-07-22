6ea1130709c9f2777c4d8a7a4531a8c3bf198aa3
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-07-22 20:47:59 +0300

    task 4

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export default function returnHowManyArguments(...params) {
+  let numOfParams = 0;
+  for (const param of params) numOfParams += 1;
+  return numOfParams;
+}
