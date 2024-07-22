467b0802177f0aa1d56c5d21d0ee05a04cfe359e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-07-23 01:14:16 +0300

    task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+
+  const employeesObj = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      const listLength = Object.keys(employeesList).length;
+      return listLength;
+    },
+  };
+  return employeesObj;
+}
