138518fda9d27b5a1e9ab6761f7843ee626aef8c
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-07-22 20:53:32 +0300

    task 5

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -3,4 +3,5 @@ export default function returnHowManyArguments(...params) {
   let numOfParams = 0;
   for (const param of params) numOfParams += 1;
   return numOfParams;
+//   return params.length;
 }
