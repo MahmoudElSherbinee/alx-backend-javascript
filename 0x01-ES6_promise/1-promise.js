a446f8ed373c50670113ec40ca6e624b6b113caf
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-07-25 00:47:31 +0300

    fix 1 * 2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  const myPromise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
-  return myPromise;
 }
